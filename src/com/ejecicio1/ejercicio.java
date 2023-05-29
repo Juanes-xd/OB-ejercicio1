ea1794d853c9f8d77dff89ca9e780376568246e8
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-05-29 14:44:34 -0500

    ejercicio1 finish 1.0

---
+++ src/com/ejecicio1/ejercicio.java
@@ -0,0 +1,16 @@
+package com.ejecicio1;
+
+public class ejercicio {
+    public static void main(String[] args) {
+        int var1 = 10;
+        double var2 = 12.45;
+        String var3 = "Hola mundo";
+        boolean var4 = true;
+        long var5 = 300;
+        short var6 = 5;
+        float var7 = 3.3f;
+        char var8 = 'e';
+        byte var9 = 1;
+        System.out.println(var1 + " " + var2 + " " + var3 + " " + var4+ " " + var5 + " " + var6 + " " + var7+ " " + var8 + " " + var9);
+    }
+}
